bb2e2e96cbf17346eb898ce930186dca364f2e32
matt.jourard <mjourard@gmail.com>
2018-10-07 02:19:49 -0400

    the memes weren't worth it

--- spec/lerp_spec.cr
+++ spec/lerp_spec.cr
@@ -8,6 +8,6 @@ describe Lerp do
     lerp(100, 0, 0.5).should eq(50)
     lerp(100, 0, 1).should eq(0)
     lerp(0, 100, 0.23).should eq(23)
-    lerp(25, 75, 0.68).as(Int32).should eq(59)
+    lerp(25, 75, 0.68).floor().should eq(59)
   end
 end
